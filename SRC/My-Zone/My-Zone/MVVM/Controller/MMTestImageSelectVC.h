5e8537d749d39a06b02bbcac32f4644a94947717
chieryw <chieryw@163.com>
2016-10-24 19:21:43 +0800

    增加图片文件上传下载

---
+++ SRC/My-Zone/My-Zone/MVVM/Controller/MMTestImageSelectVC.h
@@ -0,0 +1,13 @@
+//
+//  MMTestImageSelectVC.h
+//  My-Zone
+//
+//  Created by chiery on 2016/10/24.
+//  Copyright © 2016年 My-Zone. All rights reserved.
+//
+
+#import "MMBaseViewController.h"
+
+@interface MMTestImageSelectVC : MMBaseViewController
+
+@end
---
+++ SRC/My-Zone/My-Zone/MVVM/Controller/MMWebImageExampleVC.h
@@ -0,0 +1,13 @@
+//
+//  MMWebImageExampleVC.h
+//  My-Zone
+//
+//  Created by chiery on 2016/10/24.
+//  Copyright © 2016年 My-Zone. All rights reserved.
+//
+
+#import "MMBaseViewController.h"
+
+@interface MMWebImageExampleVC : UITableViewController
+
+@end
